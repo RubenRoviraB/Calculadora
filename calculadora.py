c9400666f024da930cb0cd8ccdef2453f1907a76
RubenRoviraB <zx20student109@colegiovalledelmiro.es>
2023-09-26 13:55:22 +0200

    dividir

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,15 @@ var2 = 0
 function sumar() {
 print(var1+var2)
 }
+
+function restar() {
+print(var1-var2)
+}
+
+function multiplicar() {
+print(var1*var2)
+}
+
+function dividir() {
+print(var1/var2)
+}
